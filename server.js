00ad67a46737e3ad91ff1abb554bdccfc615a1a7
Srikanth-15L <pandaraboinasrikanth7@gmail.com>
2026-03-18 00:57:07 +0530

    commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const nodemailer = require('nodemailer');
+require('dns').setDefaultResultOrder('ipv4first');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
